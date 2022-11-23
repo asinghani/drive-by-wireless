f0096084c2ba680cefe5906ec0d28f3ad10fca42
Anish <anish@anishsinghani.com>
2022-11-23 16:16:49 -0500

    add hardware dir

--- experimental/steering_test/main.c
+++ experimental/steering_test/main.c
@@ -2,13 +2,25 @@
 #include "pico/stdlib.h"
 #include "pico/stdio.h"
 #include "hardware/uart.h"
+#include "hardware/xosc.h"
 
 #include "steering_wheel.h"
 
 int main() {
+    xosc_init();
     stdio_init_all();
     sleep_ms(3000);
     printf("test\n");
+
+    gpio_init(0);
+    gpio_set_dir(0, true);
+
+    while (1) {
+        gpio_put(0, 0);
+        sleep_ms(500);
+        gpio_put(0, 1);
+        sleep_ms(500);
+    }
 
     steering_setup();
 
