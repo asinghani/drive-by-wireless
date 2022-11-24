ddc5c8f61108d3b589c34d69fcbf2c7dad334795
Anish <anish@anishsinghani.com>
2022-11-23 19:01:46 -0500

    build works with 3 zones

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Distributed Embedded Final Project
 
 UWB drive-by-wireless
+
+## Build Instructions
+
+1. Clone pico-sdk and pico-extras and put them in your environment variables as `PICO_SDK_PATH` and `PICO_EXTRAS_PATH`. Also clone FreeRTOS-Kernel, `smp` branch and put its path as `FREERTOS_KERNEL_PATH`
+
+2. 
---
+++ firmware/main.c
@@ -0,0 +1,125 @@
+/* Scheduler include files. */
+#include "FreeRTOS.h"
+#include "task.h"
+#include "semphr.h"
+
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "pico/stdio.h"
+#include "hardware/uart.h"
+#include "hardware/xosc.h"
+
+/* Set mainCREATE_SIMPLE_BLINKY_DEMO_ONLY to one to run the simple blinky demo,
+or 0 to run the more comprehensive test and demo application. */
+
+/*-----------------------------------------------------------*/
+
+/*
+ * Configure the hardware as necessary to run this demo.
+ */
+static void prvSetupHardware( void );
+
+/*
+ * main_blinky() is used when mainCREATE_SIMPLE_BLINKY_DEMO_ONLY is set to 1.
+ * main_full() is used when mainCREATE_SIMPLE_BLINKY_DEMO_ONLY is set to 0.
+ */
+extern void main_blinky( void );
+
+/* Prototypes for the standard FreeRTOS callback/hook functions implemented
+within this file. */
+void vApplicationMallocFailedHook( void );
+void vApplicationIdleHook( void );
+void vApplicationStackOverflowHook( TaskHandle_t pxTask, char *pcTaskName );
+void vApplicationTickHook( void );
+
+/*-----------------------------------------------------------*/
+
+void vLaunch( void)
+{
+        main_blinky();
+}
+
+#define xassert(cond) if (!(cond)) { goodbye(#cond, __LINE__); }
+
+void goodbye(const char* cond, int line) {
+    while (1) {
+        printf("Assertion '%s' failed on line %d. Please add more magic.\n", cond, line);
+        sleep_ms(2000);
+    }
+}
+
+int main( void )
+{
+    xosc_init();
+    stdio_init_all();
+    sleep_ms(2000);
+    printf("HELLO\n");
+
+    xassert(portSUPPORT_SMP == 1);
+    xassert(configNUM_CORES == 2);
+
+    prvSetupHardware();
+
+    printf("Starting %s\n", ZONE_NAME);
+    vLaunch();
+
+    return 0;
+}
+/*-----------------------------------------------------------*/
+
+static void prvSetupHardware( void )
+{
+    gpio_init(PICO_DEFAULT_LED_PIN);
+    gpio_set_dir(PICO_DEFAULT_LED_PIN, 1);
+    gpio_put(PICO_DEFAULT_LED_PIN, !PICO_DEFAULT_LED_PIN_INVERTED);
+}
+/*-----------------------------------------------------------*/
+
+void vApplicationMallocFailedHook( void )
+{
+    /* Called if a call to pvPortMalloc() fails because there is insufficient
+    free memory available in the FreeRTOS heap.  pvPortMalloc() is called
+    internally by FreeRTOS API functions that create tasks, queues, software
+    timers, and semaphores.  The size of the FreeRTOS heap is set by the
+    configTOTAL_HEAP_SIZE configuration constant in FreeRTOSConfig.h. */
+
+    /* Force an assert. */
+    configASSERT( ( volatile void * ) NULL );
+}
+/*-----------------------------------------------------------*/
+
+void vApplicationStackOverflowHook( TaskHandle_t pxTask, char *pcTaskName )
+{
+    ( void ) pcTaskName;
+    ( void ) pxTask;
+
+    /* Run time stack overflow checking is performed if
+    configCHECK_FOR_STACK_OVERFLOW is defined to 1 or 2.  This hook
+    function is called if a stack overflow is detected. */
+
+    /* Force an assert. */
+    configASSERT( ( volatile void * ) NULL );
+}
+/*-----------------------------------------------------------*/
+
+void vApplicationIdleHook( void )
+{
+    volatile size_t xFreeHeapSpace;
+
+    /* This is just a trivial example of an idle hook.  It is called on each
+    cycle of the idle task.  It must *NOT* attempt to block.  In this case the
+    idle task just queries the amount of FreeRTOS heap that remains.  See the
+    memory management section on the http://www.FreeRTOS.org web site for memory
+    management options.  If there is a lot of heap memory free then the
+    configTOTAL_HEAP_SIZE value in FreeRTOSConfig.h can be reduced to free up
+    RAM. */
+    xFreeHeapSpace = xPortGetFreeHeapSize();
+
+    /* Remove compiler warning about xFreeHeapSpace being set but never used. */
+    ( void ) xFreeHeapSpace;
+}
+/*-----------------------------------------------------------*/
+
+void vApplicationTickHook( void )
+{
+}
