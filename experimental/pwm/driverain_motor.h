05bb7480907b8693ceec42377ed640179ac48bc5
Akash Arun <akasha@andrew.cmu.edu>
2022-12-03 00:36:06 -0500

    drivetrain zone fully integrated with freertos need to test it

--- experimental/pwm/driverain_motor.h
+++
@@ -1,7 +0,0 @@
-#include <stdint.h>
-
-/* Motor initialization */
-void drivetrain_motor_init();
-
-/* Speed and Brake control */
-void drivetrain_motor_set(int8_t motor_speed, bool brake_state); 
--- firmware/z_drivetrain/main_drivetrain.c
+++ firmware/z_drivetrain/main_drivetrain.c
@@ -5,7 +5,7 @@
 #include <stdio.h>
 #include <string.h>
 #include "peripherals/testpoints.h"
-#include "peripherals/drivetrain_servo.h"
+#include "peripherals/drivetrain_motor.h"
 
 struct DrivetrainState {
     int8_t motor_speed;
